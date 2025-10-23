d1becafeb6b547d5ae06662c5a1b37b12ed0dff8
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-23 15:09:42 +0100

    sort posts by asc or desc

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,12 +1,19 @@
 import { db } from "@/app/utils/utilities.js";
 import Link from "next/link";
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
   const res = await db.query(`SELECT * FROM blog_posts ORDER BY id ASC`);
   const posts = res.rows;
 
+  const pageSearchParams = await searchParams;
+  if (pageSearchParams.sortBy == "desc") {
+    posts.reverse();
+  }
+
   return (
     <div>
+      <Link href="/posts?sortBy=desc">Newest to oldest</Link>
+      <Link href="/posts?sortBy=asc">Oldest to newest</Link>
       <p>Some text that introduces the list of posts</p>
       {posts.map((post) => (
         <Link href={`/posts/${post.id}`} key={post.id}>
