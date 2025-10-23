8a4d0127a5a4b62ac6a9e1d59acef6049433d975
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-23 13:27:05 +0100

    removed .map

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -5,23 +5,24 @@ export default async function Page({ params }) {
   const { id } = await params;
 
   const res = await db.query(`SELECT * FROM blog_posts WHERE id = $1`, [id]);
-  const posts = res.rows;
+  const posts = res.rows[0];
+  console.log(posts);
 
   return (
     <div>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>By {post.author}</p>
-          <p>Posted on: date</p>
-          {/* Add <Image src={post.img_url} alt="" />*/}
-          <p className="whitespace-pre-line">{post.content}</p>
-          <p>🎬 Films to watch or avoid (you choose!):</p>
-          <p className="whitespace-pre-line">{post.recommended_films}</p>
-          <p className="italic">💭 {post.prompt}</p>
-        </div>
-      ))}
+      <div>
+        <h2>{posts.title}</h2>
+        <p>By {posts.author}</p>
+        <p>Posted on: date</p>
+        {/* Add <Image src={post.img_url} alt="" />*/}
+        <p className="whitespace-pre-line">{posts.content}</p>
+        <p>🎬 Films to watch or avoid (you choose!):</p>
+        <p className="whitespace-pre-line">{posts.recommended_films}</p>
+        <p className="italic">💭 {posts.prompt}</p>
+      </div>
       <CommentForm />
+      {/* pass in the id of post to CommentForm so it can use the id to link our new comments to the correct post */}
+      {/* Add CommentDisplay /> pass in the id of post (likewise for commentForm) to fetch correct comments*/}
     </div>
   );
 }
